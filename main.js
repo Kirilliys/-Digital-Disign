c83fc844e903d1501429591cf6b55677b81be5ef
Kirilliys <Kirilliys12@mail.ru>
2022-03-08 14:41:04 +0300

    create: Создал функцию с выводом инф о дате

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-function getDayInfo(date){
-  
+function getDayInfo(date) {
+  const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+  const months = ['Января', 'Февраля', 'Марта','Апреля','Мая','Июня','Июля','Августа','Сентябра','Октября','Ноября','Декабря']
+
+  let tmp = date.split('.')
+  date = new Date(tmp[2],tmp[1] - 1,tmp[0]); 
+
+	const week = Math.ceil((date.getDate() + date.getDay()) / 7);
+	
+
+	
+  return days[date.getDay()] + ', '+ week + ' неделя ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' года'
 }
+
+
+console.log(getDayInfo('15.12.2021'))
+console.log(getDayInfo('29.10.2021'))
+
+
+
+
+
+
+
